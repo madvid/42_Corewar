7ca4d563cc8d406bcb8d0f13fe3cda98dceee083
madvid <mdavid@student.42.fr>
2020-07-14 14:32:56 +0200

    libft: ajout de ft_itoa_base, quelques tests ont été fait, mais des tests plus conséquents devront être envisagés.

--- libft/include/libft.h
+++ libft/include/libft.h
@@ -6,7 +6,7 @@
 /*   By: mdavid <mdavid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/09 13:11:02 by mdavid            #+#    #+#             */
-/*   Updated: 2020/07/13 16:53:46 by mdavid           ###   ########.fr       */
+/*   Updated: 2020/07/14 13:30:02 by mdavid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -116,5 +116,6 @@ unsigned long long	ft_atoull(const char *str);
 int					ft_is_positive_int(char *str);
 int					ft_str_isnumber(char *number);
 void				ft_strtabldel(char ***tab);
+char				*ft_itoa_base(int nb, char* strbase, int base);
 
 #endif
